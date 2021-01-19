bf7ae06edb037e3368dffcec67eb291d7c69f6f8
my-wei <51904948+my-wei@users.noreply.github.com>
2021-01-19 18:07:31 +0800

    第六次修改
    暂无

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,15 +1,34 @@
-const ncname=`[a-zA-z_][\\-\\.0-9_a-zA-Z]*`;
-const qnameCaptrue=`((?:${ncname})\\:)?${ncname}`;
+const ncname=`[a-zA-z_][\\-\\.0-9_a-zA-Z]*`;//匹配的标签名
+const qnameCaptrue=`((?:${ncname})\\:)?${ncname}`; //my:xx 
 const starTagOpen=new RegExp(`^<${qnameCaptrue}`);//标签开头的正则，捕获的内容是标签名
 const endTag=new RegExp(`^<\\/${qnameCaptrue}[^>]*>`);//匹配标签结尾的</div>
 const attribute=/^\s*([^\s"'<>\/=]+)(?:\s*(?:"([^"]*)"+|'([^\s"'=<>']+)))?/;//匹配属性的
 const startTagClose=/^\s*(\/?)>/;//匹配标签结束的>
-const defaultTagRE=/\{\{((?:.|\r?\n)+?)\}\}/g;
+const defaultTagRE=/\{\{((?:.|\r?\n)+?)\}\}/g;//匹配{{}}
 
+function start (tagName,attrs){
+
+}
+function end(tagName){
+
+}
+function chars(text){
+
+}
 
 
 function parseHTML (html){
-
+    while (html){ //只要html不为空 字符串就一直解析
+        let textEnd= html.indexOf('<');
+        if(textEnd==0){
+            parseStartTag();
+            
+            break
+        }
+    }
+    function parseStartTag(){
+        html.match()
+    }
 }
 
 export function compileToFuncions(template){
